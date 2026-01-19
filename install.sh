e6f7694ba6b3c6edf4d3c5adf85ed0fedd01ddb3
Akarapol Kasivittayanun <akarapol@gmail.com>
2026-01-19 09:12:52 +0700

    fix: add libcairo2-dev to required libraries
    This dependency is often required for graphical rendering libraries (like Cairo) used by some Frappe applications or their dependencies.

--- install.sh
+++ install.sh
@@ -222,7 +222,7 @@ install_library() {
   sudo apt install --no-install-recommends -y \
       build-essential software-properties-common ca-certificates \
       curl wget llvm make gpg openssl sudo unzip zsh \
-      libffi-dev libnss3 libnspr4 tk-dev xvfb \
+      libcairo2-dev libffi-dev libnss3 libnspr4 tk-dev xvfb \
       libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev && \
   sudo apt autoclean -y && sudo apt autoremove -y
 
