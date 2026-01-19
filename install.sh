e15387a8150bbd4854ae2d2d38329887294e4b00
Akarapol Kasivittayanun <akarapol@gmail.com>
2026-01-19 19:51:11 +0700

    fix: update system dependencies for better repository support
    - Add apt-transport-https, gnupg, and lsb-release to ensure compatibility with external repositories.
    - Remove software-properties-common to streamline initial library installation.

--- install.sh
+++ install.sh
@@ -219,8 +219,8 @@ install_library() {
 
   sudo apt update && sudo apt upgrade -y && \
   sudo apt install --no-install-recommends -y \
-      build-essential software-properties-common pkg-config ca-certificates \
-      curl wget llvm make gpg openssl sudo unzip zsh cron \
+      build-essential pkg-config apt-transport-https ca-certificates \
+      curl wget llvm make gpg gnupg lsb-release openssl sudo unzip zsh cron \
       libcairo2-dev libffi-dev libnss3 libnspr4 tk-dev xvfb \
       libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev && \
   sudo apt autoclean -y && sudo apt autoremove -y
